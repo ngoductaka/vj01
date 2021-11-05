2c2296299fa32eabda3854b1047c831084a4826b
ducdn <ducdn@gmail.com>
2021-11-05 10:52:28 +0700

    update code

--- src/constant/endpoints.js
+++ src/constant/endpoints.js
@@ -1,16 +1,16 @@
 const root = 'https://apps.vietjack.com:8081';
 // const root = 'http://45.124.87.227:8181';
 export const endpoints = {
-    BASE_URL:  `${root}/api`,
+    BASE_URL: `${root}/api`,
     ROOT_URL: `${root}`,
-    
+
     BASE_URL_COURSE: `${root}`,
     MEDIA_URL: 'https://media.vietjack.com:6969/',
     BASE_URL_EXAM: 'https://khoahoc.vietjack.com/mobile-api/v2/exam/',
     BASE_HOI_DAP: 'https://hoidap.vietjack.com',
 
-    // hoi_dap_api: 'https://hoidapvietjack.com',
-    hoi_dap_api: 'http://test.vietjack.com:8088',
+    hoi_dap_api: 'https://hoidapvietjack.com',
+    // hoi_dap_api: 'http://test.vietjack.com:8088',
 
     BASE_COURSE_IMG: 'https://khoahoc.vietjack.com/',
     BASE_COURSE_PDF: 'https://video.vietjack.com',
