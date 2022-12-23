4abe57281cbf69103642f2bdddd72c6a9ceec53f
ducdn <ducdn@gmail.com>
2022-12-23 19:49:56 +0700

    Update code for android

--- src/screen/Auth/index.js
+++ src/screen/Auth/index.js
@@ -18,13 +18,13 @@ import { common_services } from '../../redux/services';
 import { helpers } from '../../utils/helpers';
 import { fontMaker, fontStyles } from '../../utils/fonts';
 import { get } from 'lodash';
-import KochavaTracker from 'react-native-kochava-tracker';
+// import KochavaTracker from 'react-native-kochava-tracker';
 
-// Configure
-var configMapObject = {}
-configMapObject[KochavaTracker.PARAM_ANDROID_APP_GUID_STRING_KEY] = "kovietjack-qy08u";
-configMapObject[KochavaTracker.PARAM_IOS_APP_GUID_STRING_KEY] = "kovietjackios-3wqjwujza";
-KochavaTracker.configure(configMapObject);
+// // Configure
+// var configMapObject = {}
+// configMapObject[KochavaTracker.PARAM_ANDROID_APP_GUID_STRING_KEY] = "kovietjack-qy08u";
+// configMapObject[KochavaTracker.PARAM_IOS_APP_GUID_STRING_KEY] = "kovietjackios-3wqjwujza";
+// KochavaTracker.configure(configMapObject);
 
 const Auth = (props) => {
 
